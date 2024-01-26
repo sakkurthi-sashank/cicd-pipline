d2bbf044d76ffbfdd51f465f10def261ee144365
sakkurthi-sashank <sakkurthisashank@gmail.com>
2024-01-26 21:20:37 +0530

    [chore] example cicd pipeline

--- __test__/index.test.ts
+++ __test__/index.test.ts
@@ -16,6 +16,6 @@ describe("GET /", () => {
   it("should return { Server is running 🚀 }", async () => {
     const res = await request(app).get("/");
     expect(res.status).toBe(200);
-    expect(res.body.message).toBe("Server is running 🚀");
+    expect(res.body.message).toBe("Server is running 👷🏻");
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.status(200).send({
-    message: "Server is running 🚀",
+    message: "Server is running 👷🏻",
   });
 });
 
